69ac38f36d66ec47215d98451b0d2787380b8421
Shahabaz Khan <shahabazkhan4ever@gmail.com>
2015-11-10 10:50:05 +0530

    Fixed some bugs, performance improvements
    v1.0.0 Ready for deployment :)

--- navigate.js
+++ navigate.js
@@ -6,47 +6,38 @@
  * http://opensource.org/licenses/MIT
  *
  * Github:  https://github.com/tigertooth01/Navigate.js/
- * Version: 0.0.1
+ * Version: 1.0.0
  */
 $(document).ready(function(){
 
-$('.page').first().css('margin-left','0vw');
+$('.page').first().css('-webkit-transform', 'translate3d(0vw, 0, 0)');
 });
-var _currentPage='#page1';
-var pageStack=['#page1'];//will contain the pages navigated
+var _currentPage='#dashboard';
+var pageStack=['#dashboard'];//will contain the pages navigated
 
 function navigateTo(page){
     
-    $(page).css( '-webkit-transition','margin 0.3s');//adding transition effect.
-    $(page).css('visibility','visible');//making it visible.
-    $(_currentPage).css('margin-left','-100vw');//push current page to left.
-    pageStack.push(page);//add new page to page stack.
-    _currentPage = page;//set new page as current page.
-    $(_currentPage).css('margin-left','0vw');//bring current page to front.
+    if(page!=_currentPage)
+    {
+        $(_currentPage).css('-webkit-transform', 'translate3d(-100vw, 0, 0)');//push current page to left.
+        pageStack.push(page);//add new page to page stack.
+        _currentPage = page;//set new page as current page.
+        $(_currentPage).css('-webkit-transform', 'translate3d(0vw, 0, 0)');
+        $(pageStack[pageStack.length-2]).css('-webkit-transform', 'translate3d(100vw, 0, 0)');//move previous page to right .
 
-    setTimeout(function(){ 
-        $(pageStack[pageStack.length-2]).css('visibility','hidden');//hide previous page.
-        $(pageStack[pageStack.length-2]).css( '-webkit-transition','');//removing transition effect to move it right.
-        $(pageStack[pageStack.length-2]).css('margin-left','100vw');//move previous page to right .
-    }, 300);
+    }
 }
             
-function goBack(){
+function navigateGoBack(){
     
     
     if(pageStack.length>1)
         {
             var poppedPage = pageStack.pop();//remove current page from page stack, and cache it.
-            $(_currentPage).css('margin-left','-100vw');//push current page to left.
+            $(_currentPage).css('-webkit-transform', 'translate3d(-100vw, 0, 0)');//push current page to left.
             _currentPage = pageStack[pageStack.length-1];//set current page as the last item in page stack ie. the previous page.
-            $(_currentPage).css( '-webkit-transition','margin 0.3s');//adding transition effect.
-            $(_currentPage).css('visibility','visible');//making it visible.
-            $(_currentPage).css('margin-left','0vw');//bring current page to front.
+            $(_currentPage).css('-webkit-transform', 'translate3d(0vw, 0, 0)');//bring current page to front.
+            $(poppedPage).css('-webkit-transform', 'translate3d(100vw, 0, 0)');//move cached page to right.
 
-            setTimeout(function(){ 
-                $(poppedPage).css('visibility','hidden');;//hide cached page. 
-                $(poppedPage).css( '-webkit-transition','');//removing transition effect to move it right.
-                $(poppedPage).css('margin-left','100vw');//move cached page to right.
-            }, 300);
         }
 }
