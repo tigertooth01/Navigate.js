8b33e4170eb6550d8873a9955faf4c20de93b055
Shahabaz Khan <shahabazkhan4ever@gmail.com>
2015-11-16 17:02:18 +0530

    Update navigate.js

--- navigate.js
+++ navigate.js
@@ -24,10 +24,10 @@ function navigateTo(page){
     
     if((page!=_currentPage)&&(!transLock))
         {
+            transLock = true;
             pageStack.push(page);//add new page to page stack.
             _currentPage = page;//set new page as current page.
             $(_currentPage).css('-webkit-transform', 'translate3d(0, 0, 0)');//bring current page to front.
-            transLock = true;
             setTimeout(function(){ $(pageStack[pageStack.length-2]).css('-webkit-transform', 'translate3d(100%, 0, 0)'); transLock = false;}, 400);
             //move previous page to right, after transition is over.
 
@@ -38,13 +38,13 @@ function navigateGoBack(){
     
     if((pageStack.length>1)&&(!transLock))
         {
+            transLock = true;
             var poppedPage = pageStack.pop();//remove current page from page stack, and cache it.
             _currentPage = pageStack[pageStack.length-1];//set current page as the last item in page stack ie. the previous page.
             $(_currentPage).css('-webkit-transition', 'none');//remove transition to bring up the page before previous page slides back.
             $(_currentPage).css('-webkit-transform', 'translate3d(0, 0, 0)');//bring current page to front.
             $(poppedPage).css('-webkit-transform', 'translate3d(100%, 0, 0)');//move cached page to right.
-            transLock = true;
-            setTimeout(function(){$(_currentPage).css('-webkit-transition', '0.4s'); transLock = false;}, 400);
+            setTimeout(function(){$(_currentPage).css('-webkit-transition', '0.4s'); transLock = false;}, 0);
             //attach transition property back, after transition is over.
         }
 }
