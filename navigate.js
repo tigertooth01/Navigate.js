bd51af319550ff8a87e40501a9e0e8ffbf429cda
Shahabaz Khan <shahabazkhan4ever@gmail.com>
2015-11-30 09:09:12 +0530

    Best strategy

--- navigate.js
+++ navigate.js
@@ -11,10 +11,8 @@
 
 var _currentPage='';// initialize with first page's ID
 var pageStack=[];//will contain the pages navigated
-var transLock=false;//prevention from spamming forward/back
 
 $(document).ready(function(){
-
     $('.page').first().css('-webkit-transform', 'translate3d(0, 0, 0)');
     _currentPage = '#'+$('.page').first().attr('id');//initialize with first page's ID.
     pageStack.push(_currentPage);//pushing first page's ID to pageStack.
@@ -22,30 +20,27 @@ $(document).ready(function(){
 
 function navigateTo(page){
     
-    if((page!=_currentPage)&&(!transLock))
+    if(page!=_currentPage)
         {
             transLock = true;
             pageStack.push(page);//add new page to page stack.
             _currentPage = page;//set new page as current page.
             $(_currentPage).css('-webkit-transform', 'translate3d(0, 0, 0)');//bring current page to front.
-            setTimeout(function(){ $(pageStack[pageStack.length-2]).css('-webkit-transform', 'translate3d(100%, 0, 0)'); transLock = false;}, 400);
-            //move previous page to right, after transition is over.
+            $(pageStack[pageStack.length-2]).css('-webkit-transform', 'translate3d(100%, 0, 0)');
+            //move previous page to right.
 
         }
 }
             
 function navigateGoBack(){
     
-    if((pageStack.length>1)&&(!transLock))
+    if(pageStack.length>1)
         {
             transLock = true;
             var poppedPage = pageStack.pop();//remove current page from page stack, and cache it.
             _currentPage = pageStack[pageStack.length-1];//set current page as the last item in page stack ie. the previous page.
-            $(_currentPage).css('-webkit-transition', 'none');//remove transition to bring up the page before previous page slides back.
             $(_currentPage).css('-webkit-transform', 'translate3d(0, 0, 0)');//bring current page to front.
             $(poppedPage).css('-webkit-transform', 'translate3d(100%, 0, 0)');//move cached page to right.
-            setTimeout(function(){$(_currentPage).css('-webkit-transition', '0.4s'); transLock = false;}, 0);
-            //attach transition property back, after transition is over.
         }
 }
 
